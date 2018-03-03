760175870c63fd15c0734362d0bef507d88600a0
LinuxKid21 <linuxkid21@gmail.com>
2018-03-03 14:54:22 -0800

    started adding UI

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,8 +2,14 @@
 
 #include <iostream>
 #include <vector>
+#include <functional>
+#include <string>
 #include <SFML/Graphics.hpp>
 #include <Box2D/Box2D.h>
+
+// evil global variable!
+// initialized first thing in main
+sf::Font font;
 
 class State {
 public:
@@ -16,7 +22,7 @@ public:
 class Server {
 public:
     //---------------------------------------------------------------------------------------------------- 
-    Server() : window(sf::VideoMode(192*3, 180*3), "THE GAME"), world(b2Vec2(0.0f,10.f))
+    Server(sf::RenderWindow &window) : world(b2Vec2(0.0f,10.f)), window(window)
     {
         window.setView(view);
         onStart();
@@ -105,7 +111,7 @@ private:
 
 
     //---------------------------------------------------------------------------------------------------- 
-    sf::RenderWindow window;
+    sf::RenderWindow &window;
     b2World world;
     
     const float32 timeStep = 1.0f / 60.0f;
@@ -118,9 +124,82 @@ private:
     std::vector<PhysicsRectangle> rectangleEntities;
 };
 
+
+
 int main()
 {
-    Server server;
+    if(!font.loadFromFile("FreeSans.ttf")) {
+        std::cerr << "Failed to load the font file 'FreeSans.ttf'\n";
+    }
+
+    sf::RenderWindow window(sf::VideoMode(192*3, 180*3), "THE GAME");
+    
+    class Button {
+    public:
+        Button(std::string name, std::function<std::vector<Button>()> func) {
+            name = name;
+            func = func;
+        }
+        std::string name;
+        // returns the new set of buttons (for the next menu) or empty if no change
+        std::function<std::vector<Button>()> func;
+    };
+    std::vector<Button> buttons;
+
+    buttons.push_back(Button(
+        "Play Game",
+        [&window]() -> std::vector<Button> {
+            std::cout << "Let's play!\n";
+
+            // play the game!
+            Server server(window);
+            return std::vector<Button>();
+        }
+    ));
+
+    buttons.push_back(Button(
+        "Test Functionality",
+        [](){
+            std::vector<Button> buttons;
+            std::cout << "Clicked!\n";
+            buttons.push_back(Button(
+                "Hello!",
+                [](){
+                    std::cout << "Second layer!\n";
+                    return std::vector<Button>();
+                }
+            ));
+            return buttons;
+        }
+    ));
+
+    // main menu
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            else if(event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+                // event.mouseButton.x
+            }
+        }
+
+
+        window.clear();
+        int i = 1;
+        for(auto &b : buttons) {
+            const sf::Vector2f SIZE(400, 100);
+            const int MARGIN = 10;
+            sf::RectangleShape rectangle(SIZE);
+            rectangle.setPosition(sf::Vector2f(10, MARGIN*i + SIZE.y*(i-1)));
+            window.draw(rectangle);
+            i++;
+        }
+        window.display();
+    }
+
 
 
 
