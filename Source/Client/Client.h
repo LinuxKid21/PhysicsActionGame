d431e9f4e5037a7498fabf4a4b0ac1019bd824c1
LinuxKid21 <linuxkid21@gmail.com>
2018-03-05 23:54:33 -0800

    made the clients wait to open window

--- Source/Client/Client.h
+++ Source/Client/Client.h
@@ -11,14 +11,9 @@
 
 class Client {
 public:
-    Client(std::string serverIP) : window(sf::VideoMode(192*3, 180*3), "THE GAME")
+    Client(sf::TcpSocket &socket) : window(sf::VideoMode(192*3, 180*3), "THE GAME"), socket(socket)
     {
         window.setView(view);
-        sf::Socket::Status status = socket.connect(serverIP, 54000);
-        if (status != sf::Socket::Done)
-        {
-            std::cerr << "ERROR!!!!!!!!\n";
-        }
         onStart();
         while (window.isOpen())
         {
@@ -49,8 +44,6 @@ public:
 
 private:
     void onStart() {
-        loadMainMenu();
-        
         socket.setBlocking(false);
     }
 
@@ -137,63 +130,6 @@ private:
         
     }
     
-    void loadMainMenu() {
-        std::cerr << "enter command: ";
-        std::string command;// = "create";
-        std::cin >> command;
-        
-        if(command == "create") {
-            NetworkEvent e = CREATE_GAME;
-            socket.send((char *)&e, sizeof(e));
-            
-            int32_t gameID;
-            size_t received;
-            socket.receive((char *)&gameID, sizeof(gameID), received);
-            
-            std::cerr << "joined game #" << gameID << "!\n";
-            return;
-        } else if(command == "list") {
-            NetworkEvent e = LIST_GAMES;
-            socket.send((char *)&e, sizeof(e));
-            
-            uint32_t gameCount;
-            size_t received;
-            socket.receive((char *)&gameCount, sizeof(gameCount), received);
-            
-            for(unsigned int i = 0;i < gameCount; i++) {
-                int32_t gameID;
-                socket.receive((char *)&gameID, sizeof(gameID), received);
-                std::cerr << "Found game #" << gameID << "\n";
-            }
-            
-            if(gameCount == 0) {
-                std::cerr << "no games found\n";
-            }
-        } else if(command == "join") {
-            NetworkEvent e = JOIN_GAME;
-            socket.send((char *)&e, sizeof(e));
-            
-            int32_t gameID;
-            std::cin >> gameID;
-            socket.send((char *)&gameID, sizeof(gameID));
-            
-            size_t received;
-            int32_t returnedGameID;
-            socket.receive((char *)&returnedGameID, sizeof(returnedGameID), received);
-            if(returnedGameID != gameID) {
-                std::cerr << "unable to join the game!\n";
-            } else {
-                std::cerr << "game joined\n";
-                return;
-            }
-            
-        } else {
-            std::cout << "UNKOWN COMMAND!\n";
-        }
-        
-        loadMainMenu(); // loop again!
-    }
-    
     //---------------------------------------------------------------------------------------------------- 
     sf::RenderWindow window;
     
@@ -204,7 +140,7 @@ private:
 
     std::vector<NetworkRectangle> rectangleEntities;
     
-    sf::TcpSocket socket;
+    sf::TcpSocket &socket;
     
     constexpr static size_t MAX_PACKET = 1024*1024*10; // arbitray value - 10 MB
     unsigned char *networkData = new unsigned char[MAX_PACKET]; // max network packet size is now 2048 bytes
--- Source/Client/main.cpp
+++ Source/Client/main.cpp
@@ -9,14 +9,81 @@
 
 
 
+void loadMainMenu(sf::TcpSocket &socket) {
+    std::cerr << "enter command: ";
+    std::string command;// = "create";
+    std::cin >> command;
+    
+    if(command == "create") {
+        NetworkEvent e = CREATE_GAME;
+        socket.send((char *)&e, sizeof(e));
+        
+        int32_t gameID;
+        size_t received;
+        socket.receive((char *)&gameID, sizeof(gameID), received);
+        
+        std::cerr << "joined game #" << gameID << "!\n";
+        return;
+    } else if(command == "list") {
+        NetworkEvent e = LIST_GAMES;
+        socket.send((char *)&e, sizeof(e));
+        
+        uint32_t gameCount;
+        size_t received;
+        socket.receive((char *)&gameCount, sizeof(gameCount), received);
+        
+        for(unsigned int i = 0;i < gameCount; i++) {
+            int32_t gameID;
+            socket.receive((char *)&gameID, sizeof(gameID), received);
+            std::cerr << "Found game #" << gameID << "\n";
+        }
+        
+        if(gameCount == 0) {
+            std::cerr << "no games found\n";
+        }
+    } else if(command == "join") {
+        NetworkEvent e = JOIN_GAME;
+        socket.send((char *)&e, sizeof(e));
+        
+        int32_t gameID;
+        std::cin >> gameID;
+        socket.send((char *)&gameID, sizeof(gameID));
+        
+        size_t received;
+        int32_t returnedGameID;
+        socket.receive((char *)&returnedGameID, sizeof(returnedGameID), received);
+        if(returnedGameID != gameID) {
+            std::cerr << "unable to join the game!\n";
+        } else {
+            std::cerr << "game joined\n";
+            return;
+        }
+        
+    } else {
+        std::cout << "UNKOWN COMMAND!\n";
+    }
+    
+    loadMainMenu(socket); // loop again!
+}
 
 int main(int argc, char *argv[])
 {
+    
     std::string serverIP = "127.0.0.1";
     if(argc >= 2) {
         serverIP = argv[1];
-    } 
-    Client client(serverIP);
+    }
+    
+    sf::TcpSocket socket;
+    sf::Socket::Status status = socket.connect(serverIP, 54000);
+    if (status != sf::Socket::Done)
+    {
+        std::cerr << "ERROR!!!!!!!!\n";
+    }
+    
+    loadMainMenu(socket);
+    
+    Client client(socket);
 
     return 0;
 }
